ea3ae7f54d01eb568a1fa8136f0f6968337b5898
f8full <ludos+github@ludoscity.com>
2020-04-09 11:42:25 -0400

    initial commit
    IntelliJ 2019.3.4
    -File->New->Project --> Kotlin->Mobile Android/iOS | Gradle
    -.gitignore
      -root --> for IntelliJ related stuff
      -app --> for Android Studio related stuff
      -iosApp --> for X Code related stuff
    IntelliJ 2020.1 (update became available)
    -update kotlin plugin version to 1.3.71
    
    Android Studio 3.6.2
    -incompatibility with 1.3.71 koltin plugin. Shall be fixed in future release?
    -able to build and launch Android app on a device
    
    -apply GNU LGPLv3 license
    https://choosealicense.com/licenses/lgpl-3.0/#

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,37 @@
+/*
+ *     Copyright (c) 2020. f8full https://github.com/f8full
+ *     Herdr is a privacy conscious multiplatform mobile data collector
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+}

a95874aca21ace2aeb06c823c77f204562851bec
f8full <ludos+github@ludoscity.com>
2020-04-10 14:50:35 -0400

    Common
    -add generic Response class
    -add expected Dispatcher
    
    Android
    
    -add actual Dispatcher using Coroutines
    
    iOS
    
    -add actual Dispatcher (auto generated dummy)

---
+++ app/src/commonMain/kotlin/com.ludoscity.common/base/Response.kt
@@ -0,0 +1,37 @@
+/*
+ *     Copyright (c) 2020. f8full https://github.com/f8full
+ *     Herdr is a privacy conscious multiplatform mobile data collector
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ludoscity.common.base
+
+sealed class Response<out T> {
+    class Success<out T>(val data: T) : Response<T>()
+    data class Error(
+        val exception: Throwable,
+        val code: Int? = null,
+        val error: Boolean? = null,
+        val errorCollection: List<ErrorElement>? = null,
+        val message: String? = null,
+        val method: String? = null,
+        val path: String? = null
+    ) : Response<Nothing>()
+}
+
+data class ErrorElement(
+    val message: String,
+    val path: String
+)

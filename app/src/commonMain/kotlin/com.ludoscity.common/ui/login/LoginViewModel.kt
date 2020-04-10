36933456d43f369626eed76c9b18320fdea2cddc
f8full <ludos+github@ludoscity.com>
2020-04-10 15:04:29 -0400

    Common. ViewModel
    -add ViewModel for login data
      -exposed as LiveData
    -add sealed class to handle execution state
      -asyn either from network or database
      -see https://phauer.com/2019/sealed-classes-exceptions-kotlin/

---
+++ app/src/commonMain/kotlin/com.ludoscity.common/ui/login/LoginViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ *     Copyright (c) 2020. f8full https://github.com/f8full
+ *     Herdr is a privacy conscious multiplatform mobile data collector
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ *     Copyright (c) 2020. f8full https://github.com/f8full
+ *     Herdr is a privacy conscious multiplatform mobile data collector
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ludoscity.common.ui.login
+
+import com.ludoscity.common.di.KodeinInjector
+import com.ludoscity.common.domain.usecase.login.RegisterAuthClientUseCase
+import dev.icerock.moko.mvvm.livedata.LiveData
+import dev.icerock.moko.mvvm.livedata.MutableLiveData
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import org.kodein.di.erased.instance
+
+class LoginViewModel : ViewModel() {
+
+    private val _authClientRegistrationResult =
+        MutableLiveData<AuthClientRegistrationState>(InProgressAuthClientRegistration())
+    val authClientRegistrationResult: LiveData<AuthClientRegistrationState>
+        get() = _authClientRegistrationResult
+
+    private val registerAuthClientUseCase by KodeinInjector.instance<RegisterAuthClientUseCase>()
+}

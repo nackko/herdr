a01b62e388da7b5abfe517a8baa7e916814cb329
f8full <ludos+github@ludoscity.com>
2020-04-10 15:01:42 -0400

    Dependency injection
    -declare Kodein bindings
      -my understanding is you point it towards constructors
    
    -few changes required to avoid an issue with byte code generation JVM 1.6 vs 1.8

---
+++ app/src/commonMain/kotlin/com.ludoscity.common/di/KodeinInjector.kt
@@ -0,0 +1,40 @@
+/*
+ *     Copyright (c) 2020. f8full https://github.com/f8full
+ *     Herdr is a privacy conscious multiplatform mobile data collector
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ludoscity.common.di
+
+import com.ludoscity.common.ApplicationDispatcher
+import com.ludoscity.common.data.repository.LoginRepository
+import com.ludoscity.common.domain.usecase.login.RegisterAuthClientUseCase
+import org.kodein.di.Kodein
+import org.kodein.di.erased.bind
+import org.kodein.di.erased.instance
+import org.kodein.di.erased.provider
+import org.kodein.di.erased.singleton
+import kotlin.coroutines.CoroutineContext
+import kotlin.native.concurrent.ThreadLocal
+
+@ThreadLocal
+val KodeinInjector = Kodein {
+
+    bind<CoroutineContext>() with provider { ApplicationDispatcher }
+
+    bind<RegisterAuthClientUseCase>() with singleton { RegisterAuthClientUseCase(instance()) }
+
+    bind<LoginRepository>() with provider { LoginRepository() }
+}

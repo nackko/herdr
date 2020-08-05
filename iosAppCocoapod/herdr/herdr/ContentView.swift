a7e3871e49a7ebda54d9693954d24fb3b8af3bfe
Fabrice V <ludos+github@ludoscity.com>
2020-08-05 00:36:19 -0400

    Mac build with cocoapod See: https://github.com/touchlab/KaMPKit/blob/4c56f296af74c25dc425d7bf488e7ae83aae6593/docs/IOS_PROJ_INTEGRATION.md
    - empty project created from Xcode
    - follow instructions from previous link
    - also added compile flag https://github.com/cashapp/sqldelight/issues/1442#issuecomment-523435492

---
+++ iosAppCocoapod/herdr/herdr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  herdr
+//
+//  Created by Fabrice on 2020-07-26.
+//  Copyright © 2020 Ludosicty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

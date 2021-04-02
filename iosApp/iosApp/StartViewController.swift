539ac3a225af4f7ce8d9f6116b06b8bb09b68216
f <ludos+github@ludoscity.com>
2021-04-02 16:59:55 -0400

    StartScreen UI (alpha)
    -add required resources
    -build UI via storyboard and code
    
    References
    -https://material.io/components?platform=ios
    -https://www.uicolor.io/

--- iosApp/iosApp/StartViewController.swift
+++ iosApp/iosApp/StartViewController.swift
@@ -2,16 +2,56 @@
 //  StartViewController.swift
 //  iosApp
 //
-//  Created by f. on 2021-03-26.
+//  Created by f8full on 2021-03-26.
 //
 
 import UIKit
 import MultiPlatformLibrary
+import MaterialComponents.MaterialCards
+import MaterialComponents.MaterialCards_Theming
+import MaterialComponents.MDCButton
+import MaterialComponents.MDCButton_MaterialTheming
+import MaterialComponents.MaterialShapeScheme
 
 class StartViewController: UIViewController {
     
-    @IBOutlet weak var cozySetupButton: UIButton!
+    @IBOutlet weak var cozySetupButton: MDCButton!
+    @IBOutlet weak var cozySetupCard: MDCCard!
     private var startViewModel: StartViewModel!
+    @objc var containerScheme: MDCContainerScheme
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        containerScheme = MDCContainerScheme()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        containerScheme = MDCContainerScheme()
+        
+        let colorScheme = MDCSemanticColorScheme()
+        
+        colorScheme.primaryColor = UIColor(red: 0.18, green: 0.57, blue: 0.96, alpha: 1.00)
+        colorScheme.onPrimaryColor = .white
+        
+        containerScheme.colorScheme = colorScheme
+        
+        let shapeScheme = MDCShapeScheme()
+        
+        let smallShapeCategory = MDCShapeCategory()
+        
+        let rounded24dpCorner = MDCCornerTreatment.corner(withRadius: 20, valueType: .absolute)
+        
+        smallShapeCategory?.topLeftCorner = rounded24dpCorner
+        smallShapeCategory?.bottomLeftCorner = rounded24dpCorner
+        smallShapeCategory?.topRightCorner = rounded24dpCorner
+        smallShapeCategory?.bottomRightCorner = rounded24dpCorner
+        
+        shapeScheme.smallComponentShape = smallShapeCategory!
+        
+        containerScheme.shapeScheme = shapeScheme
+        
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +68,13 @@ class StartViewController: UIViewController {
     
     func configView() {
         cozySetupButton.addTarget(self, action: #selector(didCozySetupButtonClick), for: .touchUpInside)
+        cozySetupButton.applyContainedTheme(withScheme: containerScheme)
+        
+        //the following doesn't work
+        cozySetupButton.imageView?.image = #imageLiteral(resourceName: "add_white_18pt")
+        //
+        
+        cozySetupCard.addTarget(self, action: #selector(didCozySetupButtonClick), for: .touchUpInside)
     }
     
     @objc func didCozySetupButtonClick(_ sender: UIButton) {

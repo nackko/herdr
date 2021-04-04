3e1d447d194487fb059b054a19af39f7ac29b06d
Fabrice V <ludos+github@ludoscity.com>
2021-04-04 18:28:12 -0400

    iOS UI (alpha) (#39)
    * StartScreen UI (alpha)
    
    -add required resources
    -build UI via storyboard and code
    
    References
    -https://material.io/components?platform=ios
    -https://www.uicolor.io/
    
    * DriveLogin UI (alpha)
    
    -reference: https://material.io/components/buttons/ios#outlined-button
    
    * DriveLogin UI (alpha)
    
    * Herdr UI (alpha)
    
    -resource used to convert Android assets
    https://shapeshifter.design/

--- iosApp/iosApp/StartViewController.swift
+++ iosApp/iosApp/StartViewController.swift
@@ -2,16 +2,35 @@
 //  StartViewController.swift
 //  iosApp
 //
-//  Created by f. on 2021-03-26.
+//  Created by f8full on 2021-03-26.
 //
 
 import UIKit
 import MultiPlatformLibrary
+import MaterialComponents.MaterialCards
+import MaterialComponents.MaterialCards_Theming
+import MaterialComponents.MDCButton
+import MaterialComponents.MDCButton_MaterialTheming
+import MaterialComponents.MaterialShapeScheme
 
 class StartViewController: UIViewController {
     
-    @IBOutlet weak var cozySetupButton: UIButton!
+    @IBOutlet weak var cozySetupButton: MDCButton!
+    @IBOutlet weak var cozySetupCard: MDCCard!
     private var startViewModel: StartViewModel!
+    @objc var containerScheme: MDCContainerScheme
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        containerScheme = MDCContainerScheme()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        initContainerScheme()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        containerScheme = MDCContainerScheme()
+        super.init(coder: aDecoder)
+        initContainerScheme()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +40,32 @@ class StartViewController: UIViewController {
         configView()
     }
     
+    func initContainerScheme() {
+        
+        let colorScheme = MDCSemanticColorScheme()
+        
+        colorScheme.primaryColor = UIColor(red: 0.18, green: 0.57, blue: 0.96, alpha: 1.00)
+        colorScheme.onPrimaryColor = .white
+        
+        containerScheme.colorScheme = colorScheme
+        
+        let shapeScheme = MDCShapeScheme()
+        
+        let smallShapeCategory = MDCShapeCategory()
+        
+        let rounded24dpCorner = MDCCornerTreatment.corner(withRadius: 20, valueType: .absolute)
+        
+        smallShapeCategory?.topLeftCorner = rounded24dpCorner
+        smallShapeCategory?.bottomLeftCorner = rounded24dpCorner
+        smallShapeCategory?.topRightCorner = rounded24dpCorner
+        smallShapeCategory?.bottomRightCorner = rounded24dpCorner
+        
+        shapeScheme.smallComponentShape = smallShapeCategory!
+        
+        containerScheme.shapeScheme = shapeScheme
+        
+    }
+    
     func initViewModel() {
         startViewModel = StartViewModel(eventsDispatcher: EventsDispatcher())
         startViewModel.eventsDispatcher.listener = self
@@ -28,6 +73,13 @@ class StartViewController: UIViewController {
     
     func configView() {
         cozySetupButton.addTarget(self, action: #selector(didCozySetupButtonClick), for: .touchUpInside)
+        cozySetupButton.applyContainedTheme(withScheme: containerScheme)
+        
+        //the following doesn't work
+        cozySetupButton.imageView?.image = #imageLiteral(resourceName: "add_white_18pt")
+        //
+        
+        cozySetupCard.addTarget(self, action: #selector(didCozySetupButtonClick), for: .touchUpInside)
     }
     
     @objc func didCozySetupButtonClick(_ sender: UIButton) {
